d52df1c4b293b83283d548db785f88094ca9a526
Omar Zidan <omarzidan87@outlook.com>
2023-11-29 13:46:21 +0100

    add env variables

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-// app.use((req, res, next) => {
-//   console.log(req.method, req.path);
-//   next();
-// });
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
